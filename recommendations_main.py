3cf78f668dcd95e21c2d2b06ff99ed2834063c6e
Modestas96 <jurcius44@gmail.com>
2021-09-26 12:04:45 +0100

    changed file psl names

--- recommendations_main.py
+++ recommendations_main.py
@@ -4,7 +4,17 @@ from Teacher import PSLTeacher
 predicate_file = 'Experiments/Recommendations/data/teacher/model/predicates.psl'
 rule_file = 'Experiments/Recommendations/data/teacher/model/model.psl'
 train_predicate_folder = 'Experiments/Recommendations/data/teacher/train'
+test_predicate_folder = 'Experiments/Recommendations/data/teacher/test'
 
+
+import os
+
+#[os.rename(f'{test_predicate_folder}/observations/{i}', f'{test_predicate_folder}/observations/{i.replace("_obs.txt", ".psl")}') for i in os.listdir(f'{test_predicate_folder}/observations/')]
+#[os.rename(f'{test_predicate_folder}/truths/{i}', f'{test_predicate_folder}/truths/{i.replace("_targets.txt", ".psl")}') for i in os.listdir(f'{test_predicate_folder}/truths/')]
+#[os.rename(f'{test_predicate_folder}/targets/{i}', f'{test_predicate_folder}/targets/{i.replace("_truth.txt", ".psl")}') for i in os.listdir(f'{test_predicate_folder}/targets/')]
+
+
+print('jj')
 
 psl_model = PSLTeacher()
 psl_model.build_model(predicate_file=predicate_file, rules_file=rule_file, predicate_folder=train_predicate_folder)
