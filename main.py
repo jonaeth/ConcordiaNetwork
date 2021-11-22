2634118953fdd7d51aba674afa8bc1a3eb9c20d8
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2021-11-22 20:38:01 +0000

    Cleaned up main

--- main.py
+++ main.py
@@ -7,22 +7,10 @@ from Experiments.CollectiveActivity.config_experiment1 import cfg
 from Experiments.CollectiveActivity.dataset import return_dataset
 from Concordia.ConcordiaNetwork import ConcordiaNetwork
 from torch.utils import data
-import numpy as np
-from Concordia.torch_losses import kl_divergence, cross_entropy
+from Concordia.torch_losses import cross_entropy
 import torch
 from Experiments.CollectiveActivity.concordia_config import concordia_config
 from Experiments.CollectiveActivity.CollectiveActivityCallback import CollectiveActivityCallback
-
-
-predicate_file = 'Experiments/CollectiveActivity/data/teacher/model/predicates.psl'
-rule_file = 'Experiments/CollectiveActivity/data/teacher/model/model.psl'
-train_predicate_folder = 'Experiments/CollectiveActivity/data/teacher/train'
-path_to_save_predicates = 'Experiments/CollectiveActivity/data/teacher/train'
-
-knowledge_base_factory = PredicateBuilder(path_to_save_predicates, cfg)
-
-teacher_psl = PSLTeacher(predicates_to_infer=['DOING', None], knowledge_base_factory=knowledge_base_factory, **concordia_config)
-teacher_psl.build_model()
 
 
 def convert_targets_to_right_shape(targets_actions, targets_activities):
@@ -48,14 +36,6 @@ def convert_targets_to_right_shape(targets_actions, targets_activities):
 
     return targets_actions, targets_activities
 
-
-def teacher_student_loss_function(student_predictions, teacher_predictions):
-    student_predictions_actions, student_predictions_activities = student_predictions
-    teacher_predictions = np.array([x for x in teacher_predictions[[0, 'truth']].groupby(0).apply(lambda x: np.array(x['truth'])).to_numpy()])
-    teacher_predictions = torch.Tensor(teacher_predictions)
-    kl_divergnece_loss = kl_divergence(student_predictions_actions, teacher_predictions)
-    return kl_divergnece_loss
-
 def student_target_loss_function(student_predictions, targets):
     student_predictions_actions, student_predictions_activities = student_predictions
     target_actions, target_activities = targets
@@ -78,7 +58,18 @@ def activities_accuracy(student_predictions, targets):
     activities_accuracy = activities_correct.item() / student_predictions_activities.shape[0]
     return activities_accuracy
 
+# Teacher
+predicate_file = 'Experiments/CollectiveActivity/data/teacher/model/predicates.psl'
+rule_file = 'Experiments/CollectiveActivity/data/teacher/model/model.psl'
+train_predicate_folder = 'Experiments/CollectiveActivity/data/teacher/train'
+path_to_save_predicates = 'Experiments/CollectiveActivity/data/teacher/train'
 
+knowledge_base_factory = PredicateBuilder(path_to_save_predicates, cfg)
+
+teacher_psl = PSLTeacher(predicates_to_infer=['DOING', None], knowledge_base_factory=knowledge_base_factory, **concordia_config)
+teacher_psl.build_model()
+
+# Student
 cfg.backbone='mobilenet'
 cfg.backbone='inv3'
 base_neural_network = BaseNet(cfg)
@@ -87,14 +78,13 @@ params = list(filter(lambda p: p.requires_grad, base_neural_network.parameters()
 optimizer = Adam(params, lr=cfg.train_learning_rate, weight_decay=cfg.weight_decay)
 student_nn = Student(base_neural_network, student_target_loss_function, optimizer)
 
-training_set, validation_set=return_dataset(cfg)
+# Data
+training_set, validation_set = return_dataset(cfg)
 
-training_set_loader = data.DataLoader(training_set)
-validation_set_loader = data.DataLoader(validation_set)
-
-
+# Setting metrics to be evaluated during Training and Testing
 callbacks = [CollectiveActivityCallback(cfg.log_path)]
 custom_metrics = {'actions_acc': actions_accuracy, 'activities_acc': activities_accuracy}
 
+
 concordia_network = ConcordiaNetwork(student_nn, teacher_psl, **concordia_config)
-concordia_network.fit(training_set_loader, validation_set_loader, callbacks=callbacks, metrics=custom_metrics)
+concordia_network.fit(data.DataLoader(training_set), data.DataLoader(validation_set), callbacks=callbacks, metrics=custom_metrics)
