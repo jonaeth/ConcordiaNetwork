2f64a748a320062743e5e37f7e97954a2fbcd197
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2022-01-12 12:35:16 +0000

    Cleaned up GetResult_valid

--- Experiments/DPL/dpl.py
+++ Experiments/DPL/dpl.py
@@ -97,7 +97,14 @@ valid_data = load_pickle_data(validation_file_path)
 test_data = load_pickle_data(test_file_path)
 
 # model
-model = EncoderRNN(args.embed_size, args.hidden_size, vocab_size, args.num_layer, args.cell, wordvec, args.class_label, args.initial_model)
+model = EncoderRNN(args.embed_size,
+                   args.hidden_size,
+                   vocab_size,
+                   args.num_layer,
+                   args.cell,
+                   wordvec,
+                   args.class_label,
+                   args.initial_model)
 
 if args.cuda:
     model = model.cuda()
@@ -158,6 +165,71 @@ def test():
                 args.threshold = threshold
         print_log_metrics(val_loss, accuracy, precision, recall, f1)
 
+
+def create_masks(len_text, start, end):
+    start_pos = max([start - 10, 0])
+    end_pos = min([end + 10, len_text])
+
+    mask = torch.LongTensor([1 if start <= i <= end else 0 for i in range(len_text)])
+    batch_mask = torch.LongTensor([0 if start_pos <= i < end_pos else 1 for i in range(len_text)])
+    if args.cuda:
+        mask = mask.cuda()
+        batch_mask = batch_mask.byte().cuda()
+
+    return mask, batch_mask
+
+
+def predict_label(input_data, batch_mask, mask):
+    output = model.forward((input_data[None], batch_mask[None].bool(), mask[None]))[0]
+    pred = output.data.max(1)[1]  # get the index of the max log-probability
+    prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
+
+    label = pred.cpu().numpy()[0]
+
+    if label == 0 and 1 - prob >= args.threshold:
+        label = 1
+
+    if label == 1 and prob < args.threshold:
+        label = 0
+
+    return label
+
+
+def predict_label(input_data, batch_mask, mask):
+    output = student.model.forward((input_data[None], batch_mask[None].bool(), mask[None]))[0]
+    pred = output.data.max(1)[1]  # get the index of the max log-probability
+    prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
+    label = pred.cpu().numpy()[0]
+
+    if label == 0 and 1 - prob >= args.threshold:
+        label = 1
+
+    if label == 1 and prob < args.threshold:
+        label = 0
+    prob = np.exp(output.data.cpu().numpy()[0][1])  # Prob(X=1)
+    return label, prob
+
+
+def map_text_to_vocab_indices(instance,vocab):
+    text_inc = instance['text']
+    tokens_inc = []
+    tokens_inc.extend([vocab(token) for token in text_inc])
+    tokens_inc = torch.LongTensor(tokens_inc)
+
+    if args.cuda:
+        tokens_inc = tokens_inc.cuda()
+    return tokens_inc
+
+
+def write_prediction_to_file(file, trial, text, true_label, predicted_label, predicted_probability):
+    # write the prediction to file
+    file.write(trial + "\t")
+    file.write(" ".join(text) + "\t")
+    file.write("true label:" + str(true_label) + "\t")
+    file.write("prediction:" + str(predicted_label) + "\t")
+    file.write("p(x=1):" + str(predicted_probability) + "\n")
+
+
 # test procedure
 def GetResult_valid(data, vocab, file_name):
     student.model.eval()
@@ -167,59 +239,18 @@ def GetResult_valid(data, vocab, file_name):
     for trial in data.keys():
         for i in range(len(data[trial]['inc'])):
             instance = data[trial]['inc'][i]
-            text_inc = instance['text']
-            len_text = len(text_inc)
-            tokens_inc = []
-            tokens_inc.extend([vocab(token) for token in text_inc])
-            tokens_inc = torch.LongTensor(tokens_inc)
-
-            if args.cuda:
-                tokens_inc = tokens_inc.cuda()
-            # input_data = Variable(tokens_inc, volatile=True)    BELOW is Equivalent in new torch version
-            input_data = tokens_inc
+            input_data = map_text_to_vocab_indices(instance, vocab)
 
             for item in instance['pos_neg_example']:
 
-                # vectorize the mask, for the entity
-                mask = [0 for k in range(len_text)]
-
-                # need to change the mask settings, for entity's attention: -10:10 window size
-                batch_mask = [1 for k in range(len_text)]  # ignore all first
-                start_pos = max([item[0] - 10, 0])
-                end_pos = min([item[1] + 10, len_text])
-                batch_mask[start_pos:end_pos] = [0] * (end_pos - start_pos)
-
-                for k in range(len(mask)):
-                    if (k >= item[0] and k < item[1] + 1):
-                        mask[k] = 1
-
-                mask = torch.LongTensor(mask)
-                batch_mask = torch.LongTensor(batch_mask)
-
-                if args.cuda:
-                    mask = mask.cuda()
-                    batch_mask = batch_mask.byte().cuda()
-
-                # mask = Variable(mask, volatile=True) Unnecessary in new torch version
-
-                output = model.forward((input_data[None], batch_mask[None].bool(), mask[None]))[0]
-                pred = output.data.max(1)[1]  # get the index of the max log-probability
-                prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
-
-                label = pred.cpu().numpy()[0]
-
-                if label == 0 and 1 - prob >= args.threshold:
-                    label = 1
-
-                if label == 1 and prob < args.threshold:
-                    label = 0
-
-                # write the prediction to file
-                fp.write(trial + "\t")
-                fp.write(" ".join(text_inc) + "\t")
-                fp.write("true label:" + str(item[2]) + "\t")
-                fp.write("prediction:" + str(label) + "\t")
-                fp.write("p(x=1):" + str(np.exp(output.data.cpu().numpy()[0][1])) + "\n")
+                mask, batch_mask = create_masks(len(instance['text']), item[0], item[1])
+                label, prob = predict_label(input_data, batch_mask, mask)
+                write_prediction_to_file(file=fp,
+                                         trial=trial,
+                                         text=instance['text'],
+                                         true_label=item[2],
+                                         predicted_label=label,
+                                         predicted_probability=prob)
 
     fp.close()
 
