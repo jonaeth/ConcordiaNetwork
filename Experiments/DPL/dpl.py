94e529f1e254110a351a881a7ac3e4cce316ce2b
Jonathan Feldstein <jonathan.feldstein@ed.ac.uk>
2022-01-13 19:02:13 +0000

    Cleaned up get_confidences

--- Experiments/DPL/dpl.py
+++ Experiments/DPL/dpl.py
@@ -180,22 +180,6 @@ def create_masks(len_text, start, end):
 
 
 def predict_label(input_data, batch_mask, mask):
-    output = model.forward((input_data[None], batch_mask[None].bool(), mask[None]))[0]
-    pred = output.data.max(1)[1]  # get the index of the max log-probability
-    prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
-
-    label = pred.cpu().numpy()[0]
-
-    if label == 0 and 1 - prob >= args.threshold:
-        label = 1
-
-    if label == 1 and prob < args.threshold:
-        label = 0
-
-    return label
-
-
-def predict_label(input_data, batch_mask, mask):
     output = student.model.forward((input_data[None], batch_mask[None].bool(), mask[None]))[0]
     pred = output.data.max(1)[1]  # get the index of the max log-probability
     prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
@@ -206,19 +190,21 @@ def predict_label(input_data, batch_mask, mask):
 
     if label == 1 and prob < args.threshold:
         label = 0
-    prob = np.exp(output.data.cpu().numpy()[0][1])  # Prob(X=1)
-    return label, prob
+
+    log_probabilities = output.data.cpu().numpy()
+    protein_log_probability = log_probabilities[0][1]  # Prob(X=1)
+    return label, protein_log_probability, log_probabilities
 
 
 def map_text_to_vocab_indices(instance,vocab):
-    text_inc = instance['text']
-    tokens_inc = []
-    tokens_inc.extend([vocab(token) for token in text_inc])
-    tokens_inc = torch.LongTensor(tokens_inc)
+    text = instance['text']
+    tokens = []
+    tokens.extend([vocab(token) for token in text])
+    tokens = torch.LongTensor(tokens)
 
     if args.cuda:
-        tokens_inc = tokens_inc.cuda()
-    return tokens_inc
+        tokens = tokens.cuda()
+    return tokens
 
 
 def write_prediction_to_file(file, trial, text, true_label, predicted_label, predicted_probability):
@@ -244,206 +230,141 @@ def GetResult_valid(data, vocab, file_name):
             for item in instance['pos_neg_example']:
 
                 mask, batch_mask = create_masks(len(instance['text']), item[0], item[1])
-                label, prob = predict_label(input_data, batch_mask, mask)
+                label, protein_probability, _ = predict_label(input_data, batch_mask, mask)
                 write_prediction_to_file(file=fp,
                                          trial=trial,
                                          text=instance['text'],
                                          true_label=item[2],
                                          predicted_label=label,
-                                         predicted_probability=prob)
+                                         predicted_probability=np.exp(protein_probability))
 
     fp.close()
 
 
-# test procedure
-def GetResult(data, entity_type, vocab):
-    result = deepcopy(data)
-    model.eval()
+def get_context_and_matches(instance):
+    context = " ".join(instance['text']).encode("ascii", "ignore").split()
+    matches = {}
+    # matched for inc
+    for key in instance['matched'].keys():
+        matches[key] = instance['matched'][key]
+
+    return context, matches
+
+
+def get_examples(result):
     examples = {}
-
-    confidence = {}
-    confidence['positive'] = []
-    confidence['negative'] = []
-
-    for trial in data.keys():
-
-        for i in range(len(data[trial]['inc'])):
-            instance = data[trial]['inc'][i]
-            text_inc = instance['text']
-            len_text = len(text_inc)
-            # will resave the result
-            new_instance = deepcopy(instance)
-            new_instance['matched'][entity_type] = []
-
-            tokens_inc = []
-            tokens_inc.extend([vocab(token) for token in text_inc])
-            tokens_inc = torch.LongTensor(tokens_inc)
-
-            if args.cuda:
-                tokens_inc = tokens_inc.cuda()
-            # input_data = Variable(tokens_inc, volatile=True)    BELOW equivalent
-            input_data = tokens_inc
-
-            # only care the specified entity type
-            checked_item = []
-            for item in instance['matched'][entity_type]:
-
-                if item in checked_item:
-                    continue
-
-                # add some filters here
-                if len(" ".join(text_inc[item[0]:item[1] + 1])) <= 3:
-                    continue
-
-                checked_item.append(item)
-                # vectorize the mask
-                mask = [0 for k in range(len_text)]
-
-                # need to change the mask settings, for entity's attention: -10:10 window size
-                batch_mask = [1 for k in range(len_text)]  # ignore all first
-                start_pos = max([item[0] - 10, 0])
-                end_pos = min([item[1] + 10, len_text])
-                batch_mask[start_pos:end_pos] = [0] * (end_pos - start_pos)
-
-                for k in range(len(mask)):
-                    if (k >= item[0] and k < item[1] + 1):
-                        mask[k] = 1
-
-                mask = torch.LongTensor(mask)
-                batch_mask = torch.LongTensor(batch_mask)
-
-                if args.cuda:
-                    mask = mask.cuda()
-                    batch_mask = batch_mask.byte().cuda()
-
-                # mask = Variable(mask, volatile=True) Unnecessary
-
-                output = model.forward((input_data[None], batch_mask[None], mask[None]))[0]
-                pred = output.data.max(1)[1]  # get the index of the max log-probability
-                prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
-
-                label = pred.cpu().numpy()[0]
-
-                if label == 0 and 1 - prob >= args.threshold:
-                    label = 1
-
-                if label == 1 and prob < args.threshold:
-                    label = 0
-
-                # save the result to the data type
-                new_instance['matched'][entity_type].append(
-                    (item[0], item[1], label, np.exp(output.data.cpu().numpy())))
-
-            result[trial]['inc'][i] = new_instance
-
-        for i in range(len(data[trial]['exc'])):
-            instance = data[trial]['exc'][i]
-            text_exc = instance['text']
-            len_text = len(text_exc)
-
-            # will resave the result
-            new_instance = deepcopy(instance)
-            new_instance['matched'][entity_type] = []
-
-            tokens_exc = []
-            tokens_exc.extend([vocab(token) for token in text_exc])
-            tokens_exc = torch.LongTensor(tokens_exc)
-
-            if args.cuda:
-                tokens_exc = tokens_exc.cuda()
-            # input_data = Variable(tokens_exc, volatile=True) BELOW equivalent
-            input_data = tokens_exc
-
-            # only care the specified entity type
-            checked_item = []
-            for item in instance['matched'][entity_type]:
-
-                if item in checked_item:
-                    continue
-
-                # add some filters here
-                if len(" ".join(text_exc[item[0]:item[1] + 1])) <= 3:
-                    continue
-
-                checked_item.append(item)
-
-                # vectorize the mask
-                mask = [0 for k in range(len_text)]
-
-                # need to change the mask settings, for entity's attention: -10:10 window size
-                batch_mask = [1 for k in range(len_text)]  # ignore all first
-                start_pos = max([item[0] - 10, 0])
-                end_pos = min([item[1] + 10, len_text])
-                batch_mask[start_pos:end_pos] = [0] * (end_pos - start_pos)
-
-                for k in range(len(mask)):
-                    if (k >= item[0]) and (k < item[1] + 1):
-                        mask[k] = 1
-
-                mask = torch.LongTensor(mask)
-                batch_mask = torch.LongTensor(batch_mask)
-
-                if args.cuda:
-                    mask = mask.cuda()
-                    batch_mask = batch_mask.byte().cuda()
-
-                # mask = Variable(mask, volatile=True) Unnecessary
-
-                output = model.forward((input_data[None], batch_mask[None], mask[None]))[0]
-                pred = output.data.max(1)[1]  # get the index of the max log-probability
-                prob = np.exp(output.data.max(1)[0].cpu().numpy()[0])
-
-                # save the result to the data type
-                label = pred.cpu().numpy()[0]
-
-                if label == 0 and 1 - prob >= args.threshold:
-                    label = 1
-
-                if label == 1 and prob < args.threshold:
-                    label = 0
-
-                new_instance['matched'][entity_type].append(
-                    (item[0], item[1], label, np.exp(output.data.cpu().numpy())))
-
-            result[trial]['exc'][i] = new_instance
-
-    # prepration writing prediction to html file
     for trial in result.keys():
-
         examples[trial] = {}
         examples[trial]['inc'] = []
         examples[trial]['exc'] = []
 
         for instance in result[trial]['inc']:
-            context_inc = " ".join(instance['text']).encode("ascii", "ignore").split()
-            matches_inc = {}
-            # matched for inc
-            for key in instance['matched'].keys():
-                matches_inc[key] = instance['matched'][key]
-
+            context_inc, matches_inc = get_context_and_matches(instance)
             examples[trial]['inc'].append((context_inc, matches_inc))
 
         for instance in result[trial]['exc']:
-            context_exc = " ".join(instance['text']).encode("ascii", "ignore").split()
-            matches_exc = {}
-            # matched for inc
-            for key in instance['matched'].keys():
-                matches_exc[key] = instance['matched'][key]
-
+            context_exc, matches_exc = get_context_and_matches(instance)
             examples[trial]['exc'].append((context_exc, matches_exc))
 
+    return examples
+
+
+def sample_matches(matches, trial, context, entity_type, positive, negative, confidence):
+    for key in matches.keys():
+        if key == entity_type:
+            for item in matches[key]:
+                if item[2] == 0 and negative <= args.max_confidence_instance:
+                    confidence['negative'].append(
+                        (trial, context, item))  # remove all the other detection
+                    negative += 1
+                    # sub-sample some examples from exclusion examples
+                if item[2] == 1 and positive <= args.max_confidence_instance:
+                    confidence['positive'].append(
+                        (trial, context, item))  # remove all the other detection
+                    positive += 1
+
+
+def get_result_of_trial(instance, entity_type):
+    input_data = map_text_to_vocab_indices(instance, vocab)
+    # will resave the result
+    new_instance = deepcopy(instance)
+    new_instance['matched'][entity_type] = []
+
+    # only care the specified entity type
+    checked_item = []
+    for item in instance['matched'][entity_type]:
+
+        if item in checked_item:
+            continue
+
+        # add some filters here
+        if len(" ".join(instance['text'][item[0]:item[1] + 1])) <= 3:
+            continue
+
+        checked_item.append(item)
+
+        mask, batch_mask = create_masks(len(instance['text']), item[0], item[1])
+
+        label, _, log_probabilites = predict_label(input_data, batch_mask, mask)
+
+        # save the result to the data type
+        new_instance['matched'][entity_type].append(
+            (item[0], item[1], label, np.exp(log_probabilites)))
+
+    return new_instance
+
+
+def get_results(data, entity_type):
+    result = deepcopy(data)
+
+    for trial in data.keys():
+
+        for i in range(len(data[trial]['inc'])):
+            instance = data[trial]['inc'][i]
+            result[trial]['inc'][i] = get_result_of_trial(instance, entity_type)
+
+        for i in range(len(data[trial]['exc'])):
+            instance = data[trial]['exc'][i]
+            result[trial]['exc'][i] = get_result_of_trial(instance, entity_type)
+
+    return result
+
+
+def get_confidence_of_trial(matches, trial, context, entity_type, positive, negative):
+    negative_confidences = []
+    positive_confidences = []
+    # sub-sample some negative from inclusion examples
+    for key in matches.keys():
+        if key == entity_type:
+            for item in matches[key]:
+                if item[2] == 0 and negative <= args.max_confidence_instance:
+                    negative_confidences.append(
+                        (trial, context, item))  # remove all the other detection
+                    negative += 1
+                    # sub-sample some examples from exclusion examples
+                if item[2] == 1 and positive <= args.max_confidence_instance:
+                    positive_confidences.append(
+                        (trial, context, item))  # remove all the other detection
+                    positive += 1
+
+    return negative_confidences, positive_confidences
+
+def get_confidences(results, entity_type):  # TODO: Clean up all the loops
+    confidences = {}
+    confidences['positive'] = []
+    confidences['negative'] = []
     # sample the confidence example for evaluation
     positive = 0
     negative = 0
 
-    for trial in result.keys():
+    for trial in results.keys():
 
         random_seed = True
         if np.random.randint(10) >= 5:
             random_seed = False
 
         # now checking each instance
-        for instance in result[trial]['inc']:
+        for instance in results[trial]['inc']:
             context_inc = " ".join(instance['text']).encode("ascii", "ignore").split()
             matches_inc = {}
 
@@ -457,19 +378,16 @@ def GetResult(data, entity_type, vocab):
                     if key == entity_type:
                         for item in matches_inc[key]:
                             if item[2] == 0 and negative <= args.max_confidence_instance:
-                                confidence['negative'].append(
+                                confidences['negative'].append(
                                     (trial, context_inc, item))  # remove all the other detection
                                 negative += 1
                                 # sub-sample some examples from exclusion examples
-                for key in matches_inc.keys():
-                    if key == entity_type:
-                        for item in matches_inc[key]:
                             if item[2] == 1 and positive <= args.max_confidence_instance:
-                                confidence['positive'].append(
+                                confidences['positive'].append(
                                     (trial, context_inc, item))  # remove all the other detection
                                 positive += 1
 
-        for instance in result[trial]['exc']:
+        for instance in results[trial]['exc']:
             context_exc = " ".join(instance['text']).encode("ascii", "ignore").split()
             matches_exc = {}
 
@@ -484,20 +402,26 @@ def GetResult(data, entity_type, vocab):
                     if key == entity_type:
                         for item in matches_exc[key]:
                             if item[2] == 0 and negative <= args.max_confidence_instance:
-                                confidence['negative'].append(
+                                confidences['negative'].append(
                                     (trial, context_exc, item))  # remove all the other detection
                                 negative += 1
-
-                # sub-sample some examples from exclusion examples
-                for key in matches_exc.keys():
-                    if key == entity_type:
-                        for item in matches_exc[key]:
+                                # sub-sample some examples from exclusion examples
                             if item[2] == 1 and positive <= args.max_confidence_instance:
-                                confidence['positive'].append(
+                                confidences['positive'].append(
                                     (trial, context_exc, item))  # remove all the other detection
                                 positive += 1
+    return confidences
 
-    return examples, confidence
+
+# test procedure
+def GetResult(data, entity_type, vocab):
+    student.model.eval()
+    results = get_results(data, entity_type)
+    # prepration writing prediction to html file
+    examples = get_examples(results)
+    confidences = get_confidences(results, entity_type)
+
+    return examples, confidences
 
 
 for epoch in range(1, args.epochs + 1):
