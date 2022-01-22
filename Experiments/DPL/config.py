03ce538db8d8bc0216b0907c17610496e3d0f2b6
Modestas <mjurcius@usertesting.com>
2022-01-22 17:04:55 +0000

    fixed gpu problem

--- Experiments/DPL/config.py
+++ Experiments/DPL/config.py
@@ -1,4 +1,6 @@
 import time
+import torch
+
 time_str = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
 
 neural_network_config = {
@@ -19,7 +21,7 @@ optimiser_config = {
 }
 
 concordia_config = {
-    'gpu_device': True,
+    'gpu_device': torch.device('gpu', 0),
     'train_online': False,
     'regression': False,
     'teacher_student_distributions_comparison': [True],
