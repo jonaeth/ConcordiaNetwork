20c3890aa610ba1720e59e0cba76b8fe265c5afb
Modestas <mjurcius@usertesting.com>
2022-01-24 20:33:56 +0000

    metrics updated with 0.75 thresh

--- Experiments/DPL/metrics.py
+++ Experiments/DPL/metrics.py
@@ -1,6 +1,8 @@
 import numpy as np
 from sklearn import metrics
 import torch
+import torch.nn.functional as F
+
 
 def compute_metrics(targets, predictions, threshold=0.5):
     predictions = np.where(np.exp(predictions) > threshold, 1, 0)
@@ -17,20 +19,20 @@ def print_log_metrics(loss, accuracy, precision, recall, f1):
 
 
 def f1_score(predictions, targets):
-    f1 = metrics.f1_score(targets.argmax(axis=1).detach().cpu().numpy(), predictions[0].argmax(axis=1).detach().cpu().numpy())
+    f1 = metrics.f1_score(targets.argmax(axis=1).detach().cpu().numpy(), (F.softmax(predictions[0])[:, 1] >= 0.75).to(int).detach().cpu().numpy())
     return torch.Tensor([f1])
 
 
 def precision_score(predictions, targets):
-    precision = metrics.precision_score(targets.argmax(axis=1).detach().cpu().numpy(), predictions[0].argmax(axis=1).detach().cpu().numpy())
+    precision = metrics.precision_score(targets.argmax(axis=1).detach().cpu().numpy(), (F.softmax(predictions[0])[:, 1] >= 0.75).to(int).detach().cpu().numpy())
     return torch.Tensor([precision])
 
 
 def recall_score(predictions, targets):
-    recall = metrics.recall_score(targets.argmax(axis=1).detach().cpu().numpy(), predictions[0].argmax(axis=1).detach().cpu().numpy())
+    recall = metrics.recall_score(targets.argmax(axis=1).detach().cpu().numpy(), (F.softmax(predictions[0])[:, 1] >= 0.75).to(int).detach().cpu().numpy())
     return torch.Tensor([recall])
 
 
 def accuracy_score(predictions, targets):
-    accuracy = metrics.accuracy_score(targets.argmax(axis=1).detach().cpu().numpy(), predictions[0].argmax(axis=1).detach().cpu().numpy())
+    accuracy = metrics.accuracy_score(targets.argmax(axis=1).detach().cpu().numpy(), (F.softmax(predictions[0])[:, 1] >= 0.75).to(int).detach().cpu().numpy())
     return torch.Tensor([accuracy])
